78974a736a04287c6aaccca670f50308c27b8abd
Theo Petit <theo.p83@gmail.com>
2022-08-31 19:59:04 +0200

    init

---
+++ scripts/recipes.js
@@ -0,0 +1,16 @@
+// Add entry here to add a recipe
+var recipes_names = [
+    "pates-au-beurre"
+]
+
+
+// Actual data to use
+var recipes = [];
+
+function load_recipes(names) {
+    for (i = 0; i < names.length; i++) {
+        recipes[i] = names[i];
+    }
+}
+
+load_recipes();
