b11ff3c814cf0407f5e650242d1cf085ff735dd3
Theo Petit <theo.p83@gmail.com>
2022-09-01 09:25:58 +0200

    test for fetch

--- scripts/recipes.js
+++ scripts/recipes.js
@@ -8,18 +8,12 @@ var recipes_names = [
 var recipes = [];
 
 async function fetchRecipe(URL) {
-    const response = await fetch(URL, {
-        method: 'GET',
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-            "Access-Control-Allow-Headers": "X-Requested-With, Content-Type, Authorization"
-        }
-    });
-    return await response.json();
+    const response = await fetch(URL);
+    const recipe = await response.json();
+    console.log(recipe)
 }
 
-console.log(fetchRecipe(BASE_URL + "recipes/pates-au-beurre.json"));
+fetchRecipe(BASE_URL + "recipes/pates-au-beurre.json");
 
 function load_recipes(names) {
     for (i = 0; i < names.length; i++) {
