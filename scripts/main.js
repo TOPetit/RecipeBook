d99de67a48488a13639718da44ca9a4d4a220fb4
Theo Petit <theo.p83@gmail.com>
2022-09-01 16:48:01 +0200

    logs

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 function main(RECIPES) {
     recipesContainer = document.getElementById("recipes-container");
     for (i = 0; i < RECIPES.length; i++) {
+        console.log(RECIPES[i]);
         let tmpInnerHTML = recipesContainer.innerHTML + "<div class='tile'></div>";
+        console.log(recipesContainer.innerHTML);
         recipesContainer.innerHTML = tmpInnerHTML;
     }
 }
