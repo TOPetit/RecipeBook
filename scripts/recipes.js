5bd19cc366fac5d893ee13d3cfdcf98a9cdfa4fc
Theo Petit <theo.p83@gmail.com>
2022-09-01 09:29:40 +0200

    test

--- scripts/recipes.js
+++ scripts/recipes.js
@@ -10,16 +10,18 @@ var recipes = [];
 async function fetchRecipe(URL) {
     const response = await fetch(URL);
     const recipe = await response.json();
-    console.log(recipe)
+    return recipe;
 }
 
 fetchRecipe(BASE_URL + "recipes/pates-au-beurre.json");
 
 function load_recipes(names) {
+    let res = [];
     for (i = 0; i < names.length; i++) {
         let RECIPE_URL = BASE_URL + "recipes" + names[i] + ".json";
-
+        res[i] = fetchRecipe(RECIPE_URL)
     }
+    return res;
 }
 
-load_recipes(recipes_names);
+var RECIPES = load_recipes(recipes_names);
