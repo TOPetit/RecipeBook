da2cc789c71f7a0da9ea5b63bab2c2c6d19cf4fe
Theo Petit <theo.p83@gmail.com>
2022-09-04 12:30:40 +0200

    2eme alternative

--- scripts/recipes.js
+++ scripts/recipes.js
@@ -6,7 +6,7 @@ var recipes_names = [
 
 async function fetchRecipe(URL) {
     const response = await fetch(URL);
-    const recipe = await response.json();
+    const recipe = JSON.parse(await response.text());
     return recipe;
 }
 
@@ -14,7 +14,7 @@ async function load_recipes(names, func) {
     let res = [];
     for (i = 0; i < names.length; i++) {
         let RECIPE_URL = BASE_URL + "recipes/" + names[i] + ".json";
-        res[i] = fetch(RECIPE_URL);
+        res[i] = fetch(RECIPE_URL);;
     }
     console.log("Loaded " + names.length + " recipes.");
     func(res);
