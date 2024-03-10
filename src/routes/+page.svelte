6ab83355f30839c00fbf35b171bab2256fb20ac9
Theo Petit <theo.p83@gmail.com>
2024-03-10 17:45:33 +0100

    display brownies from github

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,29 @@
 <script lang="ts">
+    import { onMount } from 'svelte';
+    import type { Recipe } from '../types';
+
+
+    let data: Recipe = null;
+
+    onMount(async () => {
+        const response = await fetch('https://raw.githubusercontent.com/TOPetit/RecipeBook/main/recipes/brownies.json');
+        data = await response.json();
+    });
 </script>
 
-<h1>Hello</h1>
+{#if data}
+    <h2>{data.title}</h2>
+    <p>{data.description}</p>
+    <ul>
+        {#each data.ingredients as ingredient}
+            <li>{ingredient}</li>
+        {/each}
+    </ul>
+    <ol>
+        {#each data.instructions as instruction}
+            <li>{instruction}</li>
+        {/each}
+    </ol>
+{:else}
+    <p>Loading...</p>
+{/if}
---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export type Recipe = null | {
+    title: string;
+    description: string;
+    date: string;
+    prep_time: string;
+    cook_time: string;
+    total_time: string;
+    servings: string;
+    author: string;
+    ingredients: string[];
+    instructions: string[];
+    notes: string[];
+}
