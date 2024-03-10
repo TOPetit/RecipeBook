aacf9a60d9b5d29de56efc1a0160bc398a01d183
Theo Petit <theo.p83@gmail.com>
2024-03-10 18:19:49 +0100

    added a prerendered entry for each recipe

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,15 @@ import preprocess from "svelte-preprocess";
 //import adapter from "@sveltejs/adapter-auto";
 import adapter from '@sveltejs/adapter-static';
 
+// List files in the recipes directory
+import fs from 'fs';
+import path from 'path';
+const recipesDir = 'recipes';
+const recipes = fs.readdirSync(recipesDir).map(file => path.parse(file).name);
+
+// Create an entry for each recipe
+const entries = recipes.map(recipe => `/recette/${recipe}`);
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
@@ -11,6 +20,11 @@ const config = {
 			assets: 'build',
 			fallback: null
 		}),
+		prerender: {
+			// use relative URLs similar to an anchor tag <a href="/test/1"></a>
+			// do not include group layout folders in the path such as /(group)/test/1
+			entries: ['/', ...entries],
+		},
 	},
 
 	preprocess: [
