81eacac5a6504420bc9bc5faf7ffbae221c46d80
Theo Petit <theo.p83@gmail.com>
2024-03-10 18:21:32 +0100

    unused code

--- src/routes/recette/[recipe]/+page.svelte
+++ src/routes/recette/[recipe]/+page.svelte
@@ -4,22 +4,18 @@
     import type { Recipe } from '../../../types';
 
     let data: Recipe = null;
-    let known_recipe: boolean = true;
 
     onMount(async () => {
         const { params } = $page;
         const response = await fetch(`https://raw.githubusercontent.com/TOPetit/RecipeBook/main/recipes/${params.recipe}.json`);
 
-        known_recipe = response.ok;
         data = await response.json();
 
     });
 </script>
 
-{#if data && known_recipe}
+{#if data}
     <pre>{JSON.stringify(data, null, 2)}</pre>
-{:else if !known_recipe}
-    <p>Recette inconnue</p>
 {:else}
     <p>Loading...</p>
 {/if}
