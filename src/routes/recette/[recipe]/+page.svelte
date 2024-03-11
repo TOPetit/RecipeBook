690033db8fa697c88efa07b17b32975364080b88
Theo Petit <theo.p83@gmail.com>
2024-03-11 14:56:34 +0100

    updated schema and recipe

--- src/routes/recette/[recipe]/+page.svelte
+++ src/routes/recette/[recipe]/+page.svelte
@@ -1,21 +1,60 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-    import { page } from '$app/stores';
-    import type { Recipe } from '../../../types';
+    import { onMount } from "svelte";
+    import { page } from "$app/stores";
+    import type { Recipe } from "../../../types";
 
-    let data: Recipe = null;
+    let recipe: Recipe = null;
 
     onMount(async () => {
         const { params } = $page;
-        const response = await fetch(`https://raw.githubusercontent.com/TOPetit/RecipeBook/main/recipes/${params.recipe}.json`);
+        const response = await fetch(
+            `https://raw.githubusercontent.com/TOPetit/RecipeBook/main/recipes/${params.recipe}.json`,
+        );
 
-        data = await response.json();
-
+        recipe = await response.json();
     });
 </script>
 
-{#if data}
-    <pre>{JSON.stringify(data, null, 2)}</pre>
+{#if recipe}
+    <h1>{recipe.title}</h1>
+
+    <p>{recipe.description}</p>
+
+    <p>Publié le {recipe.date}</p>
+
+    <p>Préparation : {recipe.prep_time}</p>
+
+    <p>Cuisson : {recipe.cook_time}</p>
+
+    <p>Total : {recipe.total_time}</p>
+
+    <p>Pour {recipe.servings}</p>
+
+    <p>Par {recipe.author}</p>
+
+    <h2>Ingrédients</h2>
+
+    <ul>
+        {#each recipe.ingredients as ingredient}
+            <li>{ingredient}</li>
+        {/each}
+    </ul>
+
+    <h2>Instructions</h2>
+
+    <ol>
+        {#each recipe.instructions as instruction}
+            <li>{instruction}</li>
+        {/each}
+    </ol>
+
+    <h2>Notes</h2>
+
+    <ul>
+        {#each recipe.notes as note}
+            <li>{note}</li>
+        {/each}
+    </ul>
 {:else}
     <p>Loading...</p>
 {/if}
